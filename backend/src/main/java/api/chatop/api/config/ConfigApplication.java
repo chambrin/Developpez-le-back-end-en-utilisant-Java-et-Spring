ca47f53392fc51addb9e58a05ac7ca4b1bbbd257
chambrin <achambrin@outlook.fr>
2025-01-21 15:10:12 +0100

    🩹 fix application

--- backend/src/main/java/api/chatop/api/config/ConfigApplication.java
+++ backend/src/main/java/api/chatop/api/config/ConfigApplication.java
@@ -2,44 +2,46 @@ package api.chatop.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class ConfigApplication {
 
+  private final CustomUserDetailsService customUserDetailsService;
+  private final BCryptPasswordEncoder passwordEncoder;
+
+  public ConfigApplication(CustomUserDetailsService customUserDetailsService, BCryptPasswordEncoder passwordEncoder) {
+    this.customUserDetailsService = customUserDetailsService;
+    this.passwordEncoder = passwordEncoder;
+  }
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http.authorizeHttpRequests(auth -> {
-      auth.requestMatchers("/admin").hasRole("ADMIN");
-      auth.requestMatchers("/user").hasRole("USER");
-      auth.anyRequest().authenticated();
-    }).formLogin(Customizer.withDefaults()).build();
+    http
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/admin").hasRole("ADMIN")
+        .requestMatchers("/user").hasRole("USER")
+        .anyRequest().authenticated()
+      )
+      .formLogin(withDefaults());
+    return http.build();
   }
 
   @Bean
-  public UserDetailsService users() {
-    UserDetails user = User.builder()
-      .username("user")
-      .password(passwordEncoder().encode("user"))
-      .roles("USER").build();
-    UserDetails admin = User.builder()
-      .username("admin")
-      .password(passwordEncoder().encode("admin"))
-      .roles("USER", "ADMIN").build();
-    return new InMemoryUserDetailsManager(user, admin);
-  }
-
-  @Bean
-  public BCryptPasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    AuthenticationManagerBuilder authenticationManagerBuilder =
+      http.getSharedObject(AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder
+      .userDetailsService(customUserDetailsService)
+      .passwordEncoder(passwordEncoder);
+    return authenticationManagerBuilder.build();
   }
 }
--- backend/src/main/java/api/chatop/api/controllers/LoginController.java
+++ backend/src/main/java/api/chatop/api/controllers/LoginController.java
@@ -16,5 +16,4 @@ public class LoginController {
   public String GetAdmin() {
     return "Hello Admin";
   }
-
 }
